9b395e1a54c9f86f0db1ca7e31a69cff79f333fe
agarciapenas <agarciapenas@stemdo.io>
2025-05-14 00:02:56 +0200

    Parte Obligatoria ¿Terminada?, falta comprobar con la app

--- src/data_loader.py
+++ src/data_loader.py
@@ -4,7 +4,8 @@ def data_loader():
 	try:
 		df_customer = pd.read_csv("data/raw/olist_customers_dataset.csv")
 		df_orders = pd.read_csv("data/raw/olist_orders_dataset.csv")
-		return df_customer, df_orders
+		df_reviews = pd.read_csv("data/raw/olist_order_reviews_dataset.csv")
+		return df_customer, df_orders, df_reviews
 	except FileNotFoundError as e:
 		print(f"File {e} not found.")
 		return None, None
