658be54f8d7b55b68c3028279bf162e7c8e66ab9
agarciapenas <agarciapenas@stemdo.io>
2025-05-14 21:54:14 +0200

    Parte obligatoria terminada

--- src/data_loader.py
+++ src/data_loader.py
@@ -5,7 +5,10 @@ def data_loader():
 		df_customer = pd.read_csv("data/raw/olist_customers_dataset.csv")
 		df_orders = pd.read_csv("data/raw/olist_orders_dataset.csv")
 		df_reviews = pd.read_csv("data/raw/olist_order_reviews_dataset.csv")
-		return df_customer, df_orders, df_reviews
+		df_orders_items = pd.read_csv("data/raw/olist_order_items_dataset.csv")
+		df_products = pd.read_csv("data/raw/olist_products_dataset.csv")
+		df_translation = pd.read_csv("data/raw/product_category_name_translation.csv")
+		return df_customer, df_orders, df_reviews, df_orders_items, df_products, df_translation
 	except FileNotFoundError as e:
 		print(f"File {e} not found.")
-		return None, None
+		return None, None, None, None, None, None
